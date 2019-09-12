bba43d365d9bd7223cc7d298cbcb8775c10e8266
Michael Wilson <mikallwilsonn@gmail.com>
2019-09-12 15:13:24 -0400

    Scaffolded the components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
+// ----
+// Dependencies
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+// -----
+// Child Components
+import Tetris from './components/Tetris';
+
+
+// ----
+// App component
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>App</p>
     </div>
   );
 }
 
+
+// ----
+// Export App Component
 export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+// ----
+// Dependencies
+import React from 'react';
+
+
+// ----
+// Display Component
+const Display = ({ gameOver, text }) => {
+    return (
+        <div>
+            {text}
+        </div>
+    );
+}
+
+
+// ----
+// Export Display Component
+export default Display;
